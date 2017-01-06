b08bf0b2c5a0e488ad6b814a4ade1e669702c347
Worker <aochen_lee@sina.com>
2017-01-06 16:25:48 +0800

    调整框架后完整项目（缓存有问题）： 1.Django缓存：    1)此次采用的是本地缓存，默认使用本地过程、内存缓存，之后又导入ETag，在浏览器内部缓存内存，但并未实现；始终产生下图的错误（见图片P36-Cache未加入缓存设置）；    2)在1)问题的基础上，网上有两种方案：        <1>是缺少环境变量，在系统中添加环境变量:DJANGO_SETTINGS_MODULE=placeholder.setting           这种方案后续报错：ImportError:Cannot import 'cache'.        <2>下载Xadmin插件；但文中报出的错误并未提及；且报错提示与settings.CACHES中DEFAULT_CACHE_ALIAS有关；    之后我尝试设置使用后端缓存，（我忘了提示错误是什么了。我再试一次，后补注）    3)在2)的基础上，我尝试不缓存，直接每次生成，将P29-30的有关缓存的相关代码删掉，启动后提示Bad Requested. 2)html文件{% load staticfiles %}后HTML文件头报错……出错。

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,17 +1,25 @@
+import hashlib
 import os
 import sys
 
 from django.conf import settings
 from django.conf.urls import url
+from django.core.cache import cache
+from django.core.urlresolvers import reverse
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
+from django.views.decorators.http import etag
 from django import forms
 
 from io import BytesIO
 from PIL import Image, ImageDraw
 
+
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
-SECRET_KEY = os.environ.get('SECRET_KEY', 'on(5mnk2x+3=*so^_iif6epm6^chr)6lh*^gq%q%+8c^yp1=#l')
+SECRET_KEY = os.environ.get('SECRET_KEY',
+                            'on(5mnk2x+3=*so^_iif6epm6^chr)6lh*^gq%q%+8c^yp1=#l')
+BASE_DIR = os.path.dirname(__file__)
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
@@ -24,6 +32,19 @@ settings.configure(
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
         ),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+    ),
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': (os.path.join(BASE_DIR, 'templates'),),
+        },
+    ),
+    STATICFILES_DIRS=(
+        os.path.join(BASE_DIR, 'static'),
+    ),
+    STATIC_URL='/static/',
 )
 
 class ImageForm(forms.Form):
@@ -35,19 +56,28 @@ class ImageForm(forms.Form):
         """Generate an image of the given type and return as raw bytes."""
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
-        image = Image.new('RGB', (width, height))
-        draw = ImageDraw.Draw(image)
-        text = '{} X {}'.format(width, height)
-        textwidth, textheight = draw.textsize(text)
-        if textwidth < width and textheight < height:
-            texttop = (height - textheight) // 2
-            textleft = (width - textwidth) // 2
-            draw.text((textleft, texttop), text, fill=(255, 255, 255))
-        content = BytesIO()
-        image.save(content, image_format)
-        content.seek(0)
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
         return content
 
+def generate_etag(request, width, height):
+    content = 'placeholder: {0} x {1}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+@etag(generate_etag)
 def placeholder(request, width, height):
     form = ImageForm({'height': height, 'width': width})
     if form.is_valid():
@@ -57,7 +87,11 @@ def placeholder(request, width, height):
         return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
-    return HttpResponse('Hello World')
+    example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+    context = {
+        'example': request.build_absolute_uri(example)
+    }
+    return render(request, 'home.html', context)
 
 urlpatterns = {
     url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
