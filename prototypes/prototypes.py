2456a247587ecee777b3382744c367acf1c12164
Worker <aochen_lee@sina.com>
2017-01-08 16:39:51 +0800

    静态页面（P39-45）： <1>基础模板，所有框架在这部分应该用法是一致的；需要注意的是.html文件中导入页面结构的用法； <2>静态页面生成，一部分是prototypes中对存放页面原型文件的设置；一方面是在views.py文件中对页面文件，绝对路径的返回调用safe_join().

---
+++ prototypes/prototypes.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from django.conf import settings
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'f%p(ijjx#f5f*od5ho%bdn8-m=svu0+0=4td)b2w0^)6a_ue9v')
+BASE_DIR = os.path.dirname(__file__)
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY=SECRET_KEY,
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'sitebuilder',
+    ),
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+        },
+    ),
+    STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+)
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
