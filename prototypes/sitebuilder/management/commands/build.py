d2cb6d1096e30b2c9fa9706d9d38d79c3910e437
Worker <aochen_lee@sina.com>
2017-01-09 11:21:29 +0800

    哈希文件（P57）: _build文件夹下css、javaScript 文件都生成相应的哈希文件，相应的.html文件中也会引用相应的哈希文件名。

--- prototypes/sitebuilder/management/commands/build.py
+++ prototypes/sitebuilder/management/commands/build.py
@@ -21,6 +21,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Request pages and build output."""
+        settings.DEBUG = False
+
         if args:
             pages = args
             available = list(get_pages())
