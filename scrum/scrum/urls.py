8028bc52a08c550b7239de93aa823cdb7a6cac7d
Worker <aochen_lee@sina.com>
2017-01-11 15:09:16 +0800

    构建API（P75-85）： 直接采用Django-rest-framework来构建API框架，API视图依赖Markdown将文档字符串转换为页面。

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from rest_framework.authtoken.views import obtain_auth_token
+
+from board.urls import router
 
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/', include(router.urls)),
 ]
