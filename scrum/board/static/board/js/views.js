4f7522b0602c4b43942585dc0c0fbd3312a1130f
Worker <aochen_lee@sina.com>
2017-01-13 15:27:06 +0800

    登录视图1：页面显示存在问题 12号路由设置和基础视图中最后对.html文件的修改应该是有问题的！ 每一个<script></script>中包含的的js脚本文件信息，在相应的路由下，却不显示页面 #home-template 和 #login-template 是存在的，路由创建没有问题，视图views.js在已经缓存的情况下，会报出304错误，提示：NOT MODIFIED。但是在，清除缓存重新导入后，依旧无法显示相应的页面。不知道问题在哪!
    这次提交包括问题还有相应的登录视图以及校验

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -3,7 +3,40 @@
  */
 
 (function ($, Backbone, _, app) {
+
+    var TemplateView = Backbone.View.extend({
+        templateName: '',
+        initialize: function () {
+            this.template = _.template($(this.templateName).html());
+        },
+        render: function () {
+            var context = this.getContext(),
+                html = this.template(context);
+            this.$el.html(html)
+        },
+        getContext: function () {
+            return {};
+        }
+    });
+    /**
+    var LoginView = Backbone.View.extend({
+        id: 'login',
+        templateName: '#login-template',
+        initialize: function () {
+            this.template = _.template($(this.templateName).html());
+        },
+        render: function () {
+            var context = this.getContext(),
+                html = this.template(context);
+            this.$el.html(html);
+        },
+        getContext: function () {
+            return {};
+        }
+    });
+
     var HomepageView = Backbone.View.extend({
+        id: 'home',
         templateName: '#home-template',
         initialize: function() {
             this.template = _.template($(this.templateName).html());
@@ -17,5 +50,45 @@
             return {};
         }
     });
+     */
+    /**出现重复，定义模板，调用模板，减少重复代码，以上代码用下边继承代替*/
+
+    var HomepageView = TemplateView.extend({
+        templateName: '#home-template'
+    });
+
+    var LoginView = TemplateView.extend({
+        id: 'login',
+        templateName: '#login-template',
+        events:{
+            'submit form': 'submit'
+        },
+        submit: function (event) {
+            var data = {};
+            event.preventDefault();
+            this.form = $(event.currentTarget);
+            data = {
+                username:$(':input[name="username"]', this.form).val(),
+                password:$(':input[name="password"]', this.form).val()
+            };
+            //Submit the login form
+            $.post(app.apiLogin, data)
+                .done($.proxy(this.loginSuccess, this))
+                .fail($.proxy(this.loginFailure, this));
+        },
+        loginSuccess: function (data) {
+            app.session.save(data.token);
+            this.trigger('login', data.token);
+        },
+        loginFailure: function (xhr, status, error) {
+            var errors = xhr.responseJSON;
+            this.showErrors(errors);
+        },
+        showErrors: function () {
+            // TODO: Show the errors from the response
+        }
+    });
+
     app.views.HomepageView = HomepageView;
+    app.views.LoginView = LoginView;
 })(jQuery, Backbone, _, app);
