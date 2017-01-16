81c2c88c77b861cd0f2202e410355920893c6dba
Worker <aochen_lee@sina.com>
2017-01-16 14:47:03 +0800

    认证路由，创建标头视图  尚未添加跳转地址……

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -18,40 +18,51 @@
             return {};
         }
     });
-    /**
-    var LoginView = Backbone.View.extend({
-        id: 'login',
-        templateName: '#login-template',
-        initialize: function () {
-            this.template = _.template($(this.templateName).html());
-        },
-        render: function () {
-            var context = this.getContext(),
-                html = this.template(context);
-            this.$el.html(html);
-        },
-        getContext: function () {
-            return {};
-        }
-    });
 
-    var HomepageView = Backbone.View.extend({
-        id: 'home',
-        templateName: '#home-template',
-        initialize: function() {
-            this.template = _.template($(this.templateName).html());
+    var FormView = TemplateView.extend({
+        events:{
+            'submit form': 'submit'
         },
-        render: function() {
-            var context = this.getContext(),
-            html = this.template(context);
-            this.$el.html(html);
+        errorTemplate: _.template('<span class="error"><%- msg %></span>>'),
+        clearErrors: function () {
+            $('.error', this.form).remove();
         },
-        getContext: function () {
-            return {};
+        showErrors: function (errors) {
+            _.map(errors, function (fieldErrors, name) {
+                var field = $(':input[name=' + name +']', this.form),
+                   label = $('label[for' + field.attr('id') +']', this.form);
+                if (label.length === 0){
+                    label = $('label', this.form).first();
+                }
+                function appendError(msg) {
+                    label.before(this.errorTemplate({msg: msg}));
+                }
+                _.map(fieldErrors, appendError, this);
+            }, this);
+        },
+        serializeForm: function (form) {
+            return _.object(_.map(form.serializeArray(), function (item) {
+                // convert object to tuple of (name, value)
+                return [item.name, item.value];
+            }));
+        },
+        submit: function (event) {
+            event.preventDefault();
+            this.form = $(event.currentTarget);
+            this.clearErrors();
+        },
+        failure: function (xhr, status, error) {
+            var errors = xhr.responseJSON;
+            this.showErrors(errors);
+        },
+        done: function (event) {
+            if (event){
+                event.preventDefault();
+            }
+            this.trigger('done');
+            this.remove();
         }
     });
-     */
-    /**出现重复，定义模板，调用模板，减少重复代码，以上代码用下边继承代替*/
 
     var HomepageView = TemplateView.extend({
         templateName: '#home-template'
@@ -85,10 +96,40 @@
             this.showErrors(errors);
         },
         showErrors: function () {
-            // TODO: Show the errors from the response
+            _.map(errors, function (fieldErrors, name) {
+                var field = $(':input[name=' + name + ']', this.form),
+                    label = $('label[for=' + field.attr('id') + ']', this.form);
+                if (label.length === 0){
+                    label = $('label', this.form).first();
+                }
+                function appendError(msg) {
+                    label.before(this.errorTemplate({msg: msg}));
+                }
+                _.map(fieldErrors, appendError, this);
+            }, this);
+        },
+        clearErrors: function () {
+            $('.error', this.form).remove();
         }
     });
 
+    var HeaderView = TemplateView.extend({
+        tagName: 'header',
+        templateName: '#header-template',
+        events:{
+            'click a.logout': 'logout'
+        },
+        getContext: function () {
+            return {authenticated: app.session.authenticated()};
+        },
+        logout: function (event) {
+            event.preventDefault();
+            app.session.delete();
+            window.location='/';
+        }
+    })
+
     app.views.HomepageView = HomepageView;
     app.views.LoginView = LoginView;
+    app.views.HeaderView = HeaderView;
 })(jQuery, Backbone, _, app);
